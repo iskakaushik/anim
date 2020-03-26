6d09352759365f0e5a481dfe5f4f60771a5de169
Kaushik Iska <kaushikiska@google.com>
2020-03-26 11:20:19 -0700

    add metrics for min and max latency

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tuple/tuple.dart';
 import 'input_form.dart';
 import 'model.dart';
 import 'simulations.dart';
@@ -158,10 +159,12 @@ class PipelineSimulationView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget metricsView = Container();
+    Widget inputLatencyView = Container();
     if (animationStatus == AnimationStatus.completed) {
       List<FrameMetrics> simulated =
           simulation.simulate(settings.totalNumTicks, settings);
       metricsView = MetricsView(simulated: simulated);
+      inputLatencyView = InputLatencyView(simulated: simulated);
     }
     var children = <Widget>[
       Container(
@@ -179,6 +182,7 @@ class PipelineSimulationView extends StatelessWidget {
         ),
       ),
       metricsView,
+      inputLatencyView,
       PipelineSimulator(
         settings: settings,
         tick: tick,
@@ -190,6 +194,72 @@ class PipelineSimulationView extends StatelessWidget {
       child: Column(
         children: children,
       ),
+    );
+  }
+}
+
+class InputLatencyView extends StatelessWidget {
+  const InputLatencyView({
+    Key key,
+    @required this.simulated,
+  }) : super(key: key);
+
+  final List<FrameMetrics> simulated;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<FrameMetrics> rendered =
+        simulated.where((e) => e.displayTime != null).toList();
+    final List<Tuple2<FrameMetrics, FrameMetrics>> pairs = [];
+
+    int totalMinLatency = 0, count = 0;
+    int totalMaxLatency = 0;
+    for (int i = 1; i < rendered.length; i++) {
+      pairs.add(
+          Tuple2<FrameMetrics, FrameMetrics>(rendered[i - 1], rendered[i]));
+      int minL = rendered[i].displayTime - rendered[i].buildStart;
+      int maxL = rendered[i].displayTime - rendered[i - 1].buildStart;
+      totalMinLatency += minL;
+      totalMaxLatency += maxL;
+      count++;
+    }
+    final String avgMinL = (totalMinLatency / count).toStringAsFixed(3);
+    final String avgMaxL = (totalMaxLatency / count).toStringAsFixed(3);
+
+    return Column(
+      children: <Widget>[
+        Text(
+            'Frame min and max latency (min = displayTime - buildStart, max = displayTime - prevBuildStart)'),
+        Text('[Avg min latency = $avgMinL, Avg max latency = $avgMaxL]'),
+        Container(
+          width: 500,
+          height: 250,
+          child: new charts.ScatterPlotChart(
+            <charts.Series<Tuple2<FrameMetrics, FrameMetrics>, int>>[
+              charts.Series<Tuple2<FrameMetrics, FrameMetrics>, int>(
+                id: 'min_latency_chart',
+                colorFn: (_, __) => charts.MaterialPalette.green.shadeDefault,
+                domainFn: (Tuple2<FrameMetrics, FrameMetrics> metrics, _) =>
+                    metrics.item2.buildStart,
+                measureFn: (Tuple2<FrameMetrics, FrameMetrics> metrics, _) =>
+                    metrics.item2.displayTime - metrics.item2.buildStart,
+                data: pairs,
+              ),
+              charts.Series<Tuple2<FrameMetrics, FrameMetrics>, int>(
+                id: 'max_latency_chart',
+                colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+                domainFn: (Tuple2<FrameMetrics, FrameMetrics> metrics, _) =>
+                    metrics.item2.buildStart,
+                measureFn: (Tuple2<FrameMetrics, FrameMetrics> metrics, _) =>
+                    metrics.item2.displayTime - metrics.item1.buildStart,
+                data: pairs,
+              )
+            ],
+            animate: false,
+            defaultRenderer: new charts.PointRendererConfig(),
+          ),
+        ),
+      ],
     );
   }
 }
